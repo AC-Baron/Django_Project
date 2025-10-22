313f49edf9f1205a4bdd28e0852bd874242c6631
Alex Baron <alexanderconnorbaron@gmail.com>
2025-10-22 15:17:51 +0100

    Horuku Error Fix (Hopefully)

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sw$xhk7)kmb3uud(1@ldok+z9@)*12$s8f30f7@q=tv5()i6$s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 
 # Application definition

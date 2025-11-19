20f2465e07f432dc1fc9872950ee3c2de5425766
Alex Baron <alexanderconnorbaron@gmail.com>
2025-11-19 20:16:21 +0000

    Debug False

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sw$xhk7)kmb3uud(1@ldok+z9@)*12$s8f30f7@q=tv5()i6$s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'tabletales-55ea308617c9.herokuapp.com',

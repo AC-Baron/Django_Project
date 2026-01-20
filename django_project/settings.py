4cadbd9d933d64d8acd7e4ff931a30347eacdf41
Alex Baron <alexanderconnorbaron@gmail.com>
2026-01-20 13:56:48 +0000

    Minor README Changes

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ import os
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'tabletales-55ea308617c9.herokuapp.com',

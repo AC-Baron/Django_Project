2db74f2c0134ae199416863fb4e264663ce8d30b
Alex Baron <alexanderconnorbaron@gmail.com>
2026-05-20 15:34:49 +0100

    Co Pilot Added debug message

--- tabletales/context_processors.py
+++ tabletales/context_processors.py
@@ -1,4 +1,6 @@
 from .models import Notification
+from django.conf import settings
+import os
 
 def notification_counts(request):
     if request.user.is_authenticated:
@@ -8,7 +10,15 @@ def notification_counts(request):
         unread_count = 0
         unread_notifications = []
 
+    cloud_name = settings.CLOUDINARY_STORAGE.get('CLOUD_NAME', '') or os.environ.get('CLOUDINARY_CLOUD_NAME', '')
+    cloud_key = settings.CLOUDINARY_STORAGE.get('API_KEY', '') or os.environ.get('CLOUDINARY_API_KEY', '')
+    cloud_secret = settings.CLOUDINARY_STORAGE.get('API_SECRET', '') or os.environ.get('CLOUDINARY_API_SECRET', '')
+    cloudinary_configured = bool(os.environ.get('CLOUDINARY_URL') or (cloud_name and cloud_key and cloud_secret))
+
     return {
         "unread_count": unread_count,
         "unread_notifications": unread_notifications,
+        "cloudinary_storage_backend": settings.DEFAULT_FILE_STORAGE,
+        "cloudinary_configured": cloudinary_configured,
+        "cloudinary_debug": settings.DEBUG or request.user.is_staff,
     }
